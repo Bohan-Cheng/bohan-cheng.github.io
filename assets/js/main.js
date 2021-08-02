3a32e2788ea672afb02bc5f479bc0788cd8d3c1e
Bohan Cheng <vipc.1224@live.ca>
2021-08-02 14:19:28 -0400

    misc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,67 @@
+let projectNames = [
+	"NthState",
+	"PaintMan",
+	"Dust Explosion",
+	"Q*bert",
+	"Multiplayer FPS",
+	"Ring of Fire",
+	"Viking",
+	"UE4 Networking",
+	"Ubisoft NEXT 2021",
+	"2D Weather System",
+	"Pong! 3D",
+	"Lord's Blessing",
+	"Match3",
+	"City 2",
+	"Bluestone Engine",
+	"Ball Game",
+	"Light Project",
+	"Matching Bird"
+];
+
+let videoSrcs = [
+	"https://www.youtube.com/embed/pcO76Q1Q5XM",
+	"https://www.youtube.com/embed/qCtEp_JTAik",
+	"https://www.youtube.com/embed/d6dAYU2yD0s",
+	"https://www.youtube.com/embed/i2v0LIzyGQI",
+	"https://www.youtube.com/embed/pIUiR_Ia5iw",
+	"https://www.youtube.com/embed/8Biify4rrQc",
+	"https://www.youtube.com/embed/yLaqNJtcTQg",
+	"https://www.youtube.com/embed/kvk2uDZK0LI",
+	"https://www.youtube.com/embed/BSE-Ikgyu7w",
+	"https://www.youtube.com/embed/13JcgEIhiq8",
+	"https://www.youtube.com/embed/TpxhnMYv5NE",
+	"https://www.youtube.com/embed/UuXVlQvSzz4",
+	"https://www.youtube.com/embed/LsCQdG0vHxo",
+	"https://www.youtube.com/embed/7glaygLfrIo",
+	"https://www.youtube.com/embed/vVX61_Sq3KQ",
+	"https://www.youtube.com/embed/iphHqxK29og",
+	"https://www.youtube.com/embed/1u0vV13f1J0",
+	"https://www.youtube.com/embed/cGToVIPQVls"
+	
+];
+
+let ProjectDescriptions = [
+	"This is the description for NthState, something something, blah blah blah blah, this and that, that and that and this thats right.",
+	"This is the description for PaintMan, something something, blah blah blah blah, this and that, that and that and this thats right.",
+	"This is the description for Dust Explosion, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Q*bert, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Multiplayer FPS, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Ring of Fire, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Viking, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for UE4 Networking, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Ubisoft NEXT 2021, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for 2D Weather System, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Pong! 3D, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Lord's Blessing, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Match3, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for City 2, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Bluestone Engine, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Ball Game, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Light Project, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough.",
+	"This is the description for Matching Bird, something something, blah blah blah blah, this and that, that and that and this thats right. Dust explosion is a game that blah blah blah blah here should be a lot of text and stuff, something something yes this is long enough."
+];
+
 (function($) {
 
 	var	$window = $(window),
@@ -22,33 +86,41 @@
 
 	/* --------------------- Portfolio Logic ------------------------- */ 
 	var opens = document.getElementsByClassName("video-open");
-	var videoWindows = document.getElementsByClassName("video-window");
-	var videoPanels = document.getElementsByClassName("video-panel");
-	var closes = document.getElementsByClassName("video-close");	
+
+	var projectWindow = document.getElementById("project-window");
+	var projectPanel = document.getElementById("project-panel");
+	var projectName = document.getElementById("project-name");
+	var ProjectDes = document.getElementById("project-description");
+	var videoFrame = document.getElementById("video-frame");
+	var close = document.getElementById("project-window-close");	
 
 		for(var i = 1; i <= opens.length; i++) 
 		{
 			(function () {
-				var open 		=  	opens.item(i-1);
-				var videoWindow =   videoWindows.item(i-1);
-				var videoPanel 	= 	videoPanels.item(i-1);
-				var close 		= 	closes.item(i-1);
-				
+				var open = opens.item(i-1);
+				var name = projectNames[i-1];
+				var src  = videoSrcs[i-1];
+				var desc = ProjectDescriptions[i-1];
+
 				open.addEventListener('click', ()=> {
-					videoWindow.classList.add('show');
-					videoPanel.classList.add('input');
-				})
-			
-				close.addEventListener('click', ()=> {
-					videoWindow.classList.remove('show');
-					videoPanel.classList.remove('input');
-					$("iframe").each(function() { 
-						var src= $(this).attr('src');
-						$(this).attr('src',src);  
-					});
+					projectWindow.classList.add('show');
+					projectPanel.classList.add('input');
+
+					projectName.innerHTML = name;
+					videoFrame.src = src;
+					ProjectDes.innerHTML = desc;
 				})
 			}());
 		}
+
+		close.addEventListener('click', ()=> {
+			projectWindow.classList.remove('show');
+			projectPanel.classList.remove('input');
+			$("iframe").each(function() { 
+				var src= $(this).attr('src');
+				$(this).attr('src',src);  
+			});
+		})
 	/* ----------------------------------------------------------------- */
 
 	// Nav.
